8a66639b1fe033cf629bfa9105c1d7ba68324224
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-07 01:59:18 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -2,30 +2,67 @@
 JavaScript is a scripting language you can use to make web pages interactive. It is one of the core technologies of the web, along with HTML and CSS, and is supported by all modern browsers.
 In this course, you'll learn fundamental programming concepts in JavaScript. You'll start with basic data structures like numbers and strings. Then you'll learn to work with arrays, objects, functions, loops, if/else statements, and more. */
 
-
 //Manipulate Arrays With unshift Method - 42% Complete
+
 // Setup
 const myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
+
 // Only change code below this line
 myArray.unshift(["Paul", 35]);
 
 
+
 /* Shopping List - 42% Complete
+
 Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
 The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
 ["Chocolate Bar", 15]
+
 There should be at least 5 sub-arrays in the list. */
 const myList = [];
 myList.unshift(["Chocolate Bar", 15],["Cereal Bar", 10],["Yogurt Bar", 20],["White Chocolate Bar", 5],["Water Bottle", 40]);
 
 
+
 /*Write Reusable JavaScript with Functions - 43% Complete
+
 In JavaScript, we can divide up our code into reusable parts called functions.
 Here's an example of a function:
+
 function functionName() {
   console.log("Hello World");
 }
+
 1.You can call or invoke this function by using its name followed by parentheses, like this: functionName(); Each time the function is called it will print out the message Hello World on the dev console. All of the code between the curly braces will be executed every time the function is called.
+
 2. Create a function called reusableFunction which prints the string Hi World to the dev console.
 Call the function. */
+
+function reusableFunction() {
+    console.log("Hi World");
+  }
+
+  reusableFunction();
+
+
+
+/* Passing Values to Functions with Arguments - 44% Complete
+
+Parameters are variables that act as placeholders for the values that are to be input to a function when it is called. When a function is defined, it is typically defined along with one or more parameters. The actual values that are input (or "passed") into a function when it is called are known as arguments.
+Here is a function with two parameters, param1 and param2:
+function testFun(param1, param2) {
+  console.log(param1, param2);
+}
+Then we can call testFun like this: testFun("Hello", "World");. We have passed two string arguments, Hello and World. Inside the function, param1 will equal the string Hello and param2 will equal the string World. Note that you could call testFun again with different arguments and the parameters would take on the value of the new arguments.
+Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments. */
+function functionWithArgs(argOne, argTwo) {
+    console.log(argOne + argTwo);
+  }
+  
+  functionWithArgs(1,2);
+  functionWithArgs(7,9);
+
+
+
