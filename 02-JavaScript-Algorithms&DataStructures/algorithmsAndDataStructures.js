34e80e0a851f580f2bb361d73b09cc4853c53225
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-07 17:47:07 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -484,7 +484,7 @@ function testGreaterThan(val) {
 
 
 
-  /* Comparison with the Greater Than Or Equal To Operator - 59% Complete
+  /* Comparison with the Greater Than Or Equal To Operator - 58% Complete
 The greater than or equal to operator (>=) compares the values of two numbers. If the number to the left is greater than or equal to the number to the right, it returns true. Otherwise, it returns false.
 
 Like the equality operator, the greater than or equal to operator will convert data types while comparing.
@@ -509,6 +509,33 @@ function testGreaterOrEqual(val) {
   }
   
   testGreaterOrEqual(10);
+
+
+
+  /* Comparison with the Less Than Operator - 59% Complete
+The less than operator (<) compares the values of two numbers. If the number to the left is less than the number to the right, it returns true. Otherwise, it returns false. Like the equality operator, the less than operator converts data types while comparing.
+
+Examples
+
+2   < 5 // true
+'3' < 7 // true
+5   < 5 // false
+3   < 2 // false
+'8' < 4 // false
+Add the less than operator to the indicated lines so that the return statements make sense. */
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
 
 
 
