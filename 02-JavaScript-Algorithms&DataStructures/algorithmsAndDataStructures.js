38159d28bc1ee22b42840615fc69fc27bfe27275
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-07 02:52:56 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -102,3 +102,27 @@ Variables which are declared without the let or const keywords are automatically
 1. Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
 
 2. Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords. */
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+oopsGlobal;
+
+function fun1() {
+  oopsGlobal = 5;// Assign 5 to oopsGlobal here
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+
+
+/*  */
