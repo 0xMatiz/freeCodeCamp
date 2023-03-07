4f8832e41ef265adda576c380f0b0634900588b2
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-07 17:42:08 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -409,7 +409,7 @@ function compareEquality(a, b) {
 
 
 
-/* Comparison with the Inequality Operator
+/* Comparison with the Inequality Operator - 56% Complete
 The inequality operator (!=) is the opposite of the equality operator. It means not equal and returns false where equality would return true and vice versa. Like the equality operator, the inequality operator will convert data types of values while comparing.
 
 Examples
