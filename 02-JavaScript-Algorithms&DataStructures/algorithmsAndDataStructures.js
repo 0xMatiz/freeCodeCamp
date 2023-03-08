cd12f7be28a8ee6dd02088d3b9945ebb183ef802
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-08 08:05:43 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -939,3 +939,36 @@ Write a switch statement to set answer for the following ranges:
 7-9 - High
 
 Note: You will need to have a case statement for each number in the range. */
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  answer = val;
+
+  switch (answer) {
+    case 1:
+    case 2:
+    case 3:
+      return 'Low';
+      break;
+    case 4:
+    case 5:
+    case 6:
+      return 'Mid';
+      break;
+    case 7:
+    case 8:
+    case 9:
+      return 'High';
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+
+
+/*  */
