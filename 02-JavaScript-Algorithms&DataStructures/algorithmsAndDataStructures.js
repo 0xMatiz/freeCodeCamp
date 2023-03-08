4ee838c8eaa8ff139bc78e9e01e31238bc80b538
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-08 08:17:17 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -918,7 +918,7 @@ switchOfStuff(1);
 
 
 
-/* Multiple Identical Options in Switch Statements - 68% Complete
+/* Multiple Identical Options in Switch Statements - 69% Complete
 If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered. If you have multiple inputs with the same output, you can represent them in a switch statement like this:
 
 let result = "";
@@ -963,11 +963,65 @@ function sequentialSizes(val) {
   }
 
 
-  // Only change code above this line
+  // Only change code above this line - 70% Complete
   return answer;
 }
 
 sequentialSizes(1);
+
+/* Replacing If Else Chains with Switch
+If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following:
+
+if (val === 1) {
+  answer = "a";
+} else if (val === 2) {
+  answer = "b";
+} else {
+  answer = "c";
+}
+can be replaced with:
+
+switch (val) {
+  case 1:
+    answer = "a";
+    break;
+  case 2:
+    answer = "b";
+    break;
+  default:
+    answer = "c";
+}
+Change the chained if/else if statements into a switch statement. */
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+switch (val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+    break;
+  default:
+    answer = ""
+    break;
+}
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
 
 
 
