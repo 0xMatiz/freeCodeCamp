e915fa33d02bb96e74031ab1445f039ee81cb266
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-08 19:12:57 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -1307,7 +1307,7 @@ Since he's a particularly happy dog, let's change his name to the string Happy C
 Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation. */
 
 // Setup
-const myDog = {
+const myDogTwo = {
   "name": "Coder",
   "legs": 4,
   "tails": 1,
@@ -1315,7 +1315,41 @@ const myDog = {
 };
 
 // Only change code below this line
-myDog.name = "Happy Coder";
+myDogTwo.name = "Happy Coder";
+
+
+
+/* Add New Properties to a JavaScript Object - 78% Complete
+You can add new properties to existing JavaScript objects the same way you would modify them.
+
+Here's how we would add a bark property to ourDog:
+
+ourDog.bark = "bow-wow";
+or
+
+ourDog["bark"] = "bow-wow";
+Now when we evaluate ourDog.bark, we'll get his bark, bow-wow.
+
+Example:
+
+const ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation. */
+
+const myDogThree = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDogThree.bark = "woof";
 
 
 
