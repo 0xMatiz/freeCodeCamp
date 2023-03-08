a9df5463fef4ac4b9bbde813f7d998ffc0776863
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-08 08:05:23 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -868,7 +868,7 @@ caseInSwitch(1);
 
 
 
-/* Adding a Default Option in Switch Statements - 67% Complete
+/* Adding a Default Option in Switch Statements - 68% Complete
 
 In a switch statement you may not be able to specify all possible values as case statements. Instead, you can add the default statement which will be executed if no matching case statements are found. Think of it like the final else statement in an if/else chain.
 
@@ -918,4 +918,24 @@ switchOfStuff(1);
 
 
 
-/*  */
+/* Multiple Identical Options in Switch Statements - 68% Complete
+If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered. If you have multiple inputs with the same output, you can represent them in a switch statement like this:
+
+let result = "";
+switch (val) {
+  case 1:
+  case 2:
+  case 3:
+    result = "1, 2, or 3";
+    break;
+  case 4:
+    result = "4 alone";
+}
+Cases for 1, 2, and 3 will all produce the same result.
+
+Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+
+Note: You will need to have a case statement for each number in the range. */
