7bed55c7ea3e4a6725d3bb480084e2cfaecdfc6f
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-08 09:25:59 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -1090,7 +1090,7 @@ abTest(2,2);
 
 
 
-/* Counting Cards - 72% Complete
+/* Counting Cards - 73% Complete
 In the casino game Blackjack, a player can determine whether they have an advantage on the next hand over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
 
 Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
