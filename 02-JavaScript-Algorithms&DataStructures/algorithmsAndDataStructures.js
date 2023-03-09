d012e043571bc5f3842fa717f266cd768b7a053d
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-08 21:31:44 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -1560,7 +1560,7 @@ const gloveBoxContents = myStorage.car.inside["glove box"];
 
 
 
-/* Accessing Nested Arrays - 82% COMPLETE
+/* Accessing Nested Arrays - 83% COMPLETE
 As we have seen in earlier examples, objects can contain both nested objects and nested arrays. Similar to accessing nested objects, array bracket notation can be chained to access nested arrays.
 
 Here is an example of how to access a nested array:
@@ -1610,6 +1610,57 @@ const myPlants = [
 ];
 
 const secondTree = myPlants[1].list[1];
+
+
+
+/* Record Collection - 84% COMPLETE
+You are given an object literal representing a part of your musical album collection. Each album has a unique id number as its key and several other properties. Not all albums have complete information.
+
+You start with an updateRecords function that takes an object literal, records, containing the musical album collection, an id, a prop (like artist or tracks), and a value. Complete the function using the rules below to modify the object passed to the function.
+
+Your function must always return the entire record collection object.
+If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+If value is an empty string, delete the given prop property from the album.
+Note: A copy of the recordCollection object is used for the tests. */
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 
 
 
