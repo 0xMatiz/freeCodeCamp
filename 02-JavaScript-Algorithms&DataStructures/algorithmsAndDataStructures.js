352c514b346b8d263bdfbd93419de60470277920
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-08 21:57:54 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -1732,7 +1732,7 @@ for (let i = 1; i < 6; i++) {
 
 
 
-/* Iterate Odd Numbers With a For Loop - 86% COMPLETE
+/* Iterate Odd Numbers With a For Loop - 87% COMPLETE
 For loops don't have to iterate one at a time. By changing our final-expression, we can count by even numbers.
 
 We'll start at i = 0 and loop while i < 10. We'll increment i by 2 each loop with i += 2.
@@ -1752,6 +1752,33 @@ const myArrayFour = [];
 // Only change code below this line
 for (let i = 1; i < 10; i += 2) {
   myArrayFour.push(i);
+}
+
+
+
+/* Count Backwards With a For Loop - 88% COMPLETE
+
+A for loop can also count backwards, so long as we can define the right conditions.
+
+In order to decrement by two each iteration, we'll need to change our initialization, condition, and final expression.
+
+We'll start at i = 10 and loop while i > 0. We'll decrement i by 2 each loop with i -= 2.
+
+const ourArray = [];
+
+for (let i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+ourArray will now contain [10, 8, 6, 4, 2]. Let's change our initialization and final expression so we can count backwards by twos to create an array of descending odd numbers.
+
+Push the odd numbers from 9 through 1 to myArray using a for loop. */
+
+// Setup
+const myArrayFive = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i -= 2) {
+  myArrayFive.push(i);
 }
 
 
