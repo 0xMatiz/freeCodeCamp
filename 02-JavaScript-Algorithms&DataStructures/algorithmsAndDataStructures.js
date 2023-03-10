9606a025ec1dc175b86c3e36f9a3e349944a2ff9
0xMatiz <82920805+0xMatiz@users.noreply.github.com>
2023-03-10 15:54:49 -0300

    Update algorithmsAndDataStructures.js

--- 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
+++ 02-JavaScript-Algorithms&DataStructures/algorithmsAndDataStructures.js
@@ -1844,7 +1844,7 @@ multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 
 
 
-/* Iterate with JavaScript Do...While Loops - 89% COMPLETE
+/* Iterate with JavaScript Do...While Loops - 90% COMPLETE
 
 The next type of loop you will learn is called a do...while loop. It is called a do...while loop because it will first do one pass of the code inside the loop no matter what, and then continue to run the loop while the specified condition evaluates to true.
 
@@ -1886,6 +1886,44 @@ do {
   myArray.push(i);
   ij++;
 } while (ij < 5)
+
+
+
+/* Replace Loops using Recursion - 91% COMPLETE
+
+Recursion is the concept that a function can be expressed in terms of itself. To help understand this, start by thinking about the following task: multiply the first n elements of an array to create the product of those elements. Using a for loop, you could do this:
+
+  function multiply(arr, n) {
+    let product = 1;
+    for (let i = 0; i < n; i++) {
+      product *= arr[i];
+    }
+    return product;
+  }
+However, notice that multiply(arr, n) == multiply(arr, n - 1) * arr[n - 1]. That means you can rewrite multiply in terms of itself and never need to use a loop.
+
+  function multiply(arr, n) {
+    if (n <= 0) {
+      return 1;
+    } else {
+      return multiply(arr, n - 1) * arr[n - 1];
+    }
+  }
+The recursive version of multiply breaks down like this. In the base case, where n <= 0, it returns 1. For larger values of n, it calls itself, but with n - 1. That function call is evaluated in the same way, calling multiply again until n <= 0. At this point, all the functions can return and the original multiply returns the answer.
+
+Note: Recursive functions must have a base case when they return without calling the function again (in this example, when n <= 0), otherwise they can never finish executing.
+
+Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr. */
+
+function sum(arr, n) {
+  // Only change code below this line
+ if(n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  } 
+  // Only change code above this line
+}
 
 
 
